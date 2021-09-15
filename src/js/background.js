5bffddc4381d2623402d1edea736dd62f77aac77
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-09-15 17:44:33 +0200

    fixed resizing issue

--- src/js/background.js
+++ src/js/background.js
@@ -62,8 +62,10 @@ export default {
     },
 
     resize() {
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.camera.aspect = window.innerWidth / window.innerHeight;
+        this.camera.updateProjectionMatrix();
+
+        this.renderer.setSize( window.innerWidth, window.innerHeight );
     },
 
     render(){
