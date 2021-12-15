48ae1f34630b5813170d7eb68dbbdc8a8ba400de
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-12-15 16:27:50 +0100

    better 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,18 +1,22 @@
 <template>
   <div>
+    <BoidsBackground></BoidsBackground>
     <Panel>
-      <h1>404</h1>
-      <h5>Not Found</h5>
+      <div style="position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); text-align: center;">
+        <h1>404</h1>
+        <h5>Not Found</h5>
+      </div>
     </Panel>
   </div>
 </template>
 
 <script>
 import Panel from "@/components/Panel";
+import BoidsBackground from "@/components/BoidsBackground";
 
 export default {
   name: "NotFound",
-  components: {Panel}
+  components: {BoidsBackground, Panel}
 }
 </script>
 
