e0e67804d90bb6b7842be3811137377784b4f660
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-12-17 16:57:50 +0100

    new home page

---
+++ src/components/FeaturedProjectsPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <Panel>
+    <span v-for="project in projects" :key="project.name" :project="project">
+      <router-link :to="{ name: 'project', params: { name: project.name } }" style="text-decoration: none !important;">
+        <div class="card">
+          <div class="cardThumbnail" :style="{backgroundImage: `url(${project.thumbnail})`}"></div>
+          <div class="cardBody">
+            <h6>{{project.displayName}}</h6>
+            <p class="primary">{{project.summary}}</p>
+            <!--<router-link v-if="project.projectPage" :to="{ name: 'project', params: { name: project.name } }"><button>more <i class="mdi mdi-dots-horizontal"></i></button></router-link><a :href="button.url" target="_blank" v-for="button in project.buttons" :key="button.text"><button>{{button.text}} <i class="mdi mdi-arrow-top-right"></i></button></a>-->
+          </div>
+        </div>
+      </router-link>
+    </span>
+
+    <!--<div class="card">
+      <div class="cardBigButton">
+        <h1><i class="mdi mdi-dots-horizontal"></i></h1>
+      </div>
+    </div>-->
+  </Panel>
+</template>
+
+<script>
+import Panel from "@/components/Panel";
+export default {
+  name: "FeaturedProjectsPanel",
+  components: {Panel},
+  data() {
+    return {
+      projects: this.$store.state.projects.list.filter(p => p.featured)
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,9 +11,23 @@
       </div>
       <Tooltip tooltip="click me!" time="1000"><span class="hiddenButton" @click="debug">[debug]</span></Tooltip>
     </Panel>
-    <ProjectPanel v-for="project in projects" :key="project.name" :project="project"></ProjectPanel>
+    <FeaturedProjectsPanel></FeaturedProjectsPanel>
+    <Panel>
+      <h1>Projects</h1>
+      <br>
+      <span v-for="project in projects" :key="project.name" :project="project">
+        <router-link :to="{ name: 'project', params: { name: project.name } }" style="text-decoration: none !important;">
+          <h6><i class="mdi mdi-arrow-right"></i> {{project.displayName}}</h6>
+          <p class="primary">{{project.summary}}</p>
+          <br>
+        </router-link>
+      </span>
+    </Panel>
     <Panel id="faq">
       <h2>FAQ</h2>
+      <br>
+      <h6>How are you?</h6>
+      <h6><i class="mdi mdi-arrow-right-bottom"></i> tired.</h6>
       <br>
       <h6>Can you fix my printer?</h6>
       <h6><i class="mdi mdi-arrow-right-bottom"></i> no.</h6>
@@ -21,20 +35,22 @@
       <h6>What are you doing?</h6>
       <h6><i class="mdi mdi-arrow-right-bottom"></i> probably programming.</h6>
       <br>
-      <h6>How are you?</h6>
-      <h6><i class="mdi mdi-arrow-right-bottom"></i> tired.</h6>
-      <br>
       <h6>Why are you?</h6>
       <h6><i class="mdi mdi-arrow-right-bottom"></i> yes.</h6>
     </Panel>
     <Panel>
       <h2>Links</h2>
-      <h6><a href="https://github.com/mymakerofficial" target="_blank">GitHub<i class="mdi mdi-arrow-top-right"></i></a></h6>
-      <h6><a href="https://my-maker.itch.io/" target="_blank">itch.io<i class="mdi mdi-arrow-top-right"></i></a></h6>
-      <h6><a href="https://www.youtube.com/channel/UCXv_YqyPVOSNocCS8fmu8Hw" target="_blank">YouTube<i class="mdi mdi-arrow-top-right"></i></a></h6>
-      <h6><Tooltip tooltip="Please tell me why my stuff is always offline" time="2500"><a href="http://status.maiker.de" target="_blank">Status Page<i class="mdi mdi-arrow-top-right"></i></a></Tooltip></h6>
+      <br>
+      <h6><a href="https://github.com/mymakerofficial" target="_blank" style="text-decoration: none !important;">GitHub<i class="mdi mdi-arrow-top-right"></i></a></h6>
+      <br>
+      <h6><a href="https://my-maker.itch.io/" target="_blank" style="text-decoration: none !important;">itch.io<i class="mdi mdi-arrow-top-right"></i></a></h6>
+      <br>
+      <h6><a href="https://www.youtube.com/channel/UCXv_YqyPVOSNocCS8fmu8Hw" target="_blank" style="text-decoration: none !important;">YouTube<i class="mdi mdi-arrow-top-right"></i></a></h6>
+      <br>
+      <h6><Tooltip tooltip="Please tell me why my stuff is always offline" time="2500"><a href="http://status.maiker.de" target="_blank" style="text-decoration: none !important;">Status Page<i class="mdi mdi-arrow-top-right"></i></a></Tooltip></h6>
       <br><br><br>
       <h2>Contact</h2>
+      <br>
       <h6><a>root[at]maiker.de</a></h6>
     </Panel>
   </div>
@@ -42,14 +58,14 @@
 
 <script>
 import Panel from "@/components/Panel";
-import ProjectPanel from "@/components/ProjectPanel";
 import Tooltip from "@/components/Tooltip";
 import anime from "animejs";
 import BoidsBackground from "@/components/BoidsBackground";
+import FeaturedProjectsPanel from "@/components/FeaturedProjectsPanel";
 
 export default {
   name: 'Home',
-  components: {BoidsBackground, Tooltip, ProjectPanel, Panel},
+  components: {FeaturedProjectsPanel, BoidsBackground, Tooltip, Panel},
 
   data() {
     return {
