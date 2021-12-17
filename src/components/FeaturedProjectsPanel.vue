0840a27951dba5992d445459b3d30e7d99149b0f
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2021-12-17 17:39:20 +0100

    randomized position for featured project cards

--- src/components/FeaturedProjectsPanel.vue
+++ src/components/FeaturedProjectsPanel.vue
@@ -2,22 +2,15 @@
   <Panel>
     <span v-for="project in projects" :key="project.name" :project="project">
       <router-link :to="{ name: 'project', params: { name: project.name } }" style="text-decoration: none !important;">
-        <div class="card">
+        <div class="card" :style="{transform: `translate(${Math.random()*64-32}px,${Math.random()*64-32}px)`}">
           <div class="cardThumbnail" :style="{backgroundImage: `url(${project.thumbnail})`}"></div>
           <div class="cardBody">
             <h6>{{project.displayName}}</h6>
             <p class="primary">{{project.summary}}</p>
-            <!--<router-link v-if="project.projectPage" :to="{ name: 'project', params: { name: project.name } }"><button>more <i class="mdi mdi-dots-horizontal"></i></button></router-link><a :href="button.url" target="_blank" v-for="button in project.buttons" :key="button.text"><button>{{button.text}} <i class="mdi mdi-arrow-top-right"></i></button></a>-->
           </div>
         </div>
       </router-link>
     </span>
-
-    <!--<div class="card">
-      <div class="cardBigButton">
-        <h1><i class="mdi mdi-dots-horizontal"></i></h1>
-      </div>
-    </div>-->
   </Panel>
 </template>
 
