e86f1c97034a270888eaeecb9f9fdfdc584cbc96
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2022-02-08 19:27:42 +0100

    removed tags from project list

--- src/components/ProjectListPanel.vue
+++ src/components/ProjectListPanel.vue
@@ -14,7 +14,7 @@
         <span v-for="project in year.list" :key="project.name" :project="project">
           <MouseDistanceAnimationElement distance="200" amount="64">
             <router-link :to="{ name: 'project', params: { name: project.name } }" style="text-decoration: none !important;">
-              <h6><i class="mdi mdi-arrow-right"></i> {{project.displayName}} <span class="badge"><i class="mdi mdi-tag"></i> {{project.tags[0]}}</span> <!--<span class="badge" v-if="!(new Date() < new Date(project.timespan.release * 1000))"><i class="mdi mdi-calendar"></i> {{project.timespan.year}}</span>--> <span class="badge" v-if="new Date() < new Date(project.timespan.release * 1000)"><i class="mdi mdi-calendar"></i> coming {{ new Intl.DateTimeFormat('en', { month: 'short' }).format(new Date(project.timespan.release * 1000))}} {{new Date(project.timespan.release * 1000).getFullYear()}}</span></h6>
+              <h6><i class="mdi mdi-arrow-right"></i> {{project.displayName}} <!--<span class="badge"><i class="mdi mdi-tag"></i> {{project.tags[0]}}</span>--> <!--<span class="badge" v-if="!(new Date() < new Date(project.timespan.release * 1000))"><i class="mdi mdi-calendar"></i> {{project.timespan.year}}</span>--> <span class="badge important" v-if="new Date() < new Date(project.timespan.release * 1000)"><i class="mdi mdi-calendar"></i> coming {{ new Intl.DateTimeFormat('en', { month: 'short' }).format(new Date(project.timespan.release * 1000))}} {{new Date(project.timespan.release * 1000).getFullYear()}}</span></h6>
               <p class="primary">{{project.summary}}</p>
               <br>
             </router-link>
